0f748af1cacee58c0c0e5b0b3b0db2069e7d553f
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2023-03-10 21:20:52 +0530

    feat: refactor models

--- collabland_action_python_template/hello_action/main.py
+++ collabland_action_python_template/hello_action/main.py
@@ -1,65 +1,11 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
-from typing import List
 from discord.enums import InteractionType, AppCommandType, AppCommandOptionType
+from ..models.metadata import Metadata
 
 hello_action_router = APIRouter(
     prefix="/hello-action",
     tags=["hello-action"],
 )
-
-
-# The mini-app version model
-class Version(BaseModel):
-    name: str
-
-
-# The mini-app supported interactions model
-class SupportedInteractions(BaseModel):
-    type: int
-    names: List[str]
-
-
-# The mini-app application command metadata model
-class ApplicationCommandMetadata(BaseModel):
-    name: str
-    shortName: str
-
-
-# The mini-app application command option data model
-class OptionsData(BaseModel):
-    name: str
-    description: str
-    type: int
-    required: bool
-
-
-# The mini-app application commands model
-class ApplicationCommands(BaseModel):
-    metadata: ApplicationCommandMetadata
-    name: str
-    type: int
-    description: str
-    options: List[OptionsData]
-
-
-# The mini-app manifest model
-class Manifest(BaseModel):
-    appId: str
-    developer: str
-    name: str
-    platforms: List[str]
-    shortName: str
-    version: Version
-    website: str
-    description: str
-
-
-# The mini-app metadata model
-class Metadata(BaseModel):
-    manifest: Manifest
-    supportedInteractions: List[SupportedInteractions]
-    applicationCommands: List[ApplicationCommands]
 
 
 @hello_action_router.get("/metadata")
---
+++ collabland_action_python_template/models/metadata.py
@@ -0,0 +1,55 @@
+from typing import List
+from pydantic import BaseModel
+
+
+# The mini-app version model
+class Version(BaseModel):
+    name: str
+
+
+# The mini-app supported interactions model
+class SupportedInteractions(BaseModel):
+    type: int
+    names: List[str]
+
+
+# The mini-app application command metadata model
+class ApplicationCommandMetadata(BaseModel):
+    name: str
+    shortName: str
+
+
+# The mini-app application command option data model
+class OptionsData(BaseModel):
+    name: str
+    description: str
+    type: int
+    required: bool
+
+
+# The mini-app application commands model
+class ApplicationCommands(BaseModel):
+    metadata: ApplicationCommandMetadata
+    name: str
+    type: int
+    description: str
+    options: List[OptionsData]
+
+
+# The mini-app manifest model
+class Manifest(BaseModel):
+    appId: str
+    developer: str
+    name: str
+    platforms: List[str]
+    shortName: str
+    version: Version
+    website: str
+    description: str
+
+
+# The mini-app metadata model
+class Metadata(BaseModel):
+    manifest: Manifest
+    supportedInteractions: List[SupportedInteractions]
+    applicationCommands: List[ApplicationCommands]
