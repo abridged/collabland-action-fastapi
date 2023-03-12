a1cf60d9bff90902d9235619e4b6e14d85903607
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2023-03-12 09:21:56 +0530

    fix: buttons to use discord.py

--- collabland_action_fastapi/button_action/main.py
+++ collabland_action_fastapi/button_action/main.py
@@ -8,6 +8,7 @@ from discord.enums import (
     ComponentType,
 )
 from ..models.metadata import Metadata
+from discord.ui import Button, View
 
 button_action_router = APIRouter(
     prefix="/button-action",
@@ -52,25 +53,22 @@ async def post_button_action_interaction(req: Dict[str, Any]):
     parsed_req = dict(req)
     interaction_type = int(parsed_req.get("type"))
     if interaction_type == InteractionType.application_command.value:
+        button = Button(
+            style=ButtonStyle.green,
+            label="Click Me!",
+            custom_id="test-button",
+            disabled=False,
+            row=1,
+        )
+        view = View()
+        view.add_item(button)
+        components = view.to_components()
         return {
             "type": InteractionResponseType.channel_message.value,
             "data": {
                 "flags": 1 << 6,
                 "content": "Click the button below, to test the interaction!",
-                "components": [
-                    {
-                        "type": ComponentType.action_row.value,
-                        "components": [
-                            {
-                                "style": ButtonStyle.green.value,
-                                "label": "Click Me!",
-                                "custom_id": "test-button",
-                                "disabled": False,
-                                "type": ComponentType.button.value,
-                            },
-                        ],
-                    }
-                ],
+                "components": components,
             },
         }
     elif interaction_type == InteractionType.component.value:
